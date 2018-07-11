c7029ac7cad6036c9d5683c6890f24008cb9087b
brendanposehn <31631286+brendanposehn@users.noreply.github.com>
2018-07-10 20:05:40 -0700

    Created config file
    For storing all constants

---
+++ robo_fett_V2/configs.h
@@ -0,0 +1,35 @@
+#ifndef ROBOFETT_CONFIGS_H
+#define ROBOFETT_CONFIGS_H
+
+//overall use
+#define DEBUG() 0
+#define LOOP_DELAY() 10
+
+//drivetrain
+#define L_MOTOR
+#define R_MOTOR
+#define L_HANDICAP
+#define R_HANDICAP
+#define ENCODER_WHEEL_RATIO
+#define WHEEL_DIAM
+#define 
+
+
+//tapefollowing
+#define PROPORTIONAL
+#define DERIVITIVE
+#define TAPEFOLLOW_SPEED1
+#define TAPEFOLLOW_SPEED2
+#define TAPEFOLLOW_GAIN1
+#define TAPEFOLLW_GAIN2
+//more threshhold stuff
+
+//claws
+
+
+//distances to move
+
+
+//maneuvering
+
+#endif //ROBOFETT_CONFIGS_H
