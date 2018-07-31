9a1772cb918a153b491abd0520ea2913bc70bbb4
Miles Justice <justicemiles729@gmail.com>
2018-07-31 12:26:33 -0700

    added QRDDISP and QRDSET -- not quite done

--- robo_fett_V2/Main/Menu.cpp
+++ robo_fett_V2/Main/Menu.cpp
@@ -1,167 +1,256 @@
-// //menu system just using switch/case.
-// #include <LiquidCrystal.h>
-// #include "Menu.h"
-// #include <phys253.h>
+//menu system just using switch/case.
+#include <LiquidCrystal.h>
+#include "Menu.h"
+#include <phys253.h>
 
 
-// void Menu::display()
-// {
-//     switch (state_)
-//     {
-//     case S_CYCLE:
-//         Serial.println("display - S_CYCLE");
-//         switch (menuOption)
-//         {
-//         case MENU_START:
-//             Serial.println("display - MENU_START");
-//             LCD.clear(); LCD.home();
-//             LCD.setCursor(0, 0); LCD.print("I am ROBO FETT");
-//             LCD.setCursor(0, 1); LCD.print("Press START :)");
-//             break;
+void Menu::display()
+{
+    switch (state_){
+    case S_CYCLE:
+        Serial.println("display - S_CYCLE");
+        switch (menuOption_){
+        case MENU_START:
+            Serial.println("display - MENU_START");
+            LCD.clear(); LCD.home();
+            LCD.setCursor(0, 0); LCD.print("I am ROBO FETT");
+            LCD.setCursor(0, 1); LCD.print("Press START :)");
+            break;
 
-//         case MENU_PDSET: 
-//             Serial.println("display - MENU_PDSET");
-//             LCD.clear();LCD.home();
-//             LCD.setCursor(0, 0); LCD.print("set PD: <STOP>");
-//             LCD.setCursor(0, 1); LCD.print("Next: <START>");
-//             break;
+        case MENU_PDSET: 
+            Serial.println("display - MENU_PDSET");
+            LCD.clear();LCD.home();
+            LCD.setCursor(0, 0); LCD.print("set PD: <STOP>");
+            LCD.setCursor(0, 1); LCD.print("Next: <START>");
+            break;
 
-//         case MENU_NAMES: //TODO: CHANGE THIS
-//             Serial.println("display - MENU_NAMES");
-//             LCD.clear(); LCD.home();
-//             LCD.setCursor(0, 0); LCD.print("Team: STOP");
-//             LCD.setCursor(0, 1); LCD.print("Next: START");
-//             break;
+        case MENU_QRDSET: 
+            Serial.println("display - MENU_QRDSET");
+            LCD.clear(); LCD.home();
+            LCD.setCursor(0, 0); LCD.print("Set QRD: <STOP>");
+            LCD.setCursor(0, 1); LCD.print("Next: <START>");
+            break;
 
-//         default:
-//             break;
-//         }
-//         break;
+        default:
+            break;
+        }
+        break;
 
-//     case S_PDSET: 
-//         Serial.println("display - S_PDSET");
-//         switch(pdOption){
-//             case PD_KP:
-//                 kp = analogRead(KNOB)/200;
-//                 LCD.setCursor(0, 0); LCD.print("KP = "); LCD.print(kp)
-//                 LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
-//                 break;
+    case S_PDSET: 
+        Serial.println("display - S_PDSET");
+        switch(pdOption_){
+            case PD_KP:
+                kp = analogRead(KNOB)/200;
+                LCD.setCursor(0, 0); LCD.print("KP = "); LCD.print(kp);
+                LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
+                break;
 
-//             case PD_KD:
-//                 kd = analogRead(KNOB)/200;
-//                 LCD.setCursor(0, 0); LCD.print("KD = "); LCD.print(kd)
-//                 LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
-//                 break;
+            case PD_KD:
+                kd = analogRead(KNOB)/200;
+                LCD.setCursor(0, 0); LCD.print("KD = "); LCD.print(kd);
+                LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
+                break;
 
-//             case PD_GAIN:
-//                 gain = analogRead(KNOB)/200;
-//                 LCD.setCursor(0, 0); LCD.print("GAIN = "); LCD.print(kp)
-//                 LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
-//                 break;
+            case PD_GAIN:
+                gain = analogRead(KNOB)/200;
+                LCD.setCursor(0, 0); LCD.print("GAIN = "); LCD.print(kp);
+                LCD.setCursor(0, 1); LCD.print("Use KNOB to set");
+                break;
             
-//             default: break;
-//         }
-//         break;
+            default: break;
+        }
+        break;
 
-//     case S_QRDCHECK:
-//         Serial.println("display - S_QRD");
-//         LCD.clear(); LCD.home();
-//         LCD.setCursor(0, 0); 
-//         LCD.print("left = "); LCD.print(":P");
-//         LCD.print("right = "); LCD.print(":P");
-//         LCD.setCursor(0, 1); 
-//         LCD.print("edge = "); LCD.print(":P");
-//         break;
+    //TODO: put qrd vals in 
+    case S_QRDDISP:
+        Serial.println("display - S_QRDDISP");
+        switch(qrdOption_){
+            case QRD_TAPE:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("L="); LCD.print(":P");
+                LCD.print(" ML="); LCD.print(":P");
+                LCD.setCursor(0, 1);
+                LCD.print("MR="); LCD.print(":P"); 
+                LCD.print(" R="); LCD.print(":P");
+                break;
 
-//     case S_IRCHECK:
-//         Serial.println("display - S_IR");
-//         LCD.clear(); LCD.home();
-//         LCD.setCursor(0, 0); 
-//         LCD.print("IR"); LCD.print(":P");
-//         LCD.setCursor(0, 1); LCD.print("<STOP> to go back");
-//         break;
+            case QRD_EDGE:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("IN = "); LCD.print(":P");
+                LCD.setCursor(0, 1);
+                LCD.print("OUT = "); LCD.print(":P");
+                break;
 
-//     case S_EXIT:
-//         Serial.println("display - S_EXIT");
-//         LCD.clear(); LCD.home();
-//         LCD.setCursor(0, 0); LCD.print("<START> to GO!!!");
-//         LCD.setCursor(0, 1); LCD.print("<STOP> to go back");
-//         break;
+            default: break;
+        }
+        break;
 
-//     default: break;
-//     }
-// }
+    //TODO: set stuff
+    case S_QRDSET:
+        //display both, and threshold (can be set using stop)
+        switch(qrdPair_){
+            case PAIR_OUT:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("L = "); LCD.print(":P");
+                LCD.print(" R = "); LCD.print(":P");
+                LCD.setCursor(0, 1);
+                LCD.print("THRESH = "); LCD.print(":P");
+                break;
+            
+            case PAIR_IN:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("ML = "); LCD.print(":P");
+                LCD.print(" MR = "); LCD.print(":P");
+                LCD.setCursor(0, 1);
+                LCD.print("THRESH = "); LCD.print(":P");
+                break;
 
-// Menu::Menu(){
-//     state_ = S_CYCLE;
-//     menuOption = 0;
-//     pdOption = 0;
-//     counter = 0;
-//     quitMenu = false;
+            case PAIR_EDGE:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("IN = "); LCD.print(":P");
+                LCD.print(" OUT = "); LCD.print(":P");
+                LCD.setCursor(0, 1);
+                LCD.print("THRESH = "); LCD.print(":P");
+                break;
 
-//     LCD.clear(); LCD.home();
-//     LCD.setCursor(0, 0); LCD.print("Starting up");
-//     LCD.setCursor(0, 1); LCD.print("Press START");
-// }
+            case PAIR_EXIT:
+                LCD.clear(); LCD.home();
+                LCD.setCursor(0, 0); 
+                LCD.print("back: <STOP> ");
+                LCD.setCursor(0, 1);
+                break;
 
-// void Menu::handleInput(int input){
-//     switch (state_){
-//     case S_CYCLE:
-//         if (input == BTN_START)
-//             (menuOption < NUM_OPTIONS - 1) ? menuOption++ : menuOption = 0;
-//         else if (input == BTN_STOP){
-//             switch (menuOption){
-//             case MENU_START: //if it is on the start menu, do nothing
-//                 break;
+            default: break;
+        }
+        break;
 
-//             case MENU_PDSET:
-//                 //switch to PD set menu
-//                 state_ = S_PDSET;
-//                 break;
+    case S_IRCHECK:
+        Serial.println("display - S_IR");
+        LCD.clear(); LCD.home();
+        LCD.setCursor(0, 0); 
+        LCD.print("IR"); LCD.print(":P");
+        LCD.setCursor(0, 1); LCD.print("<STOP> to go back");
+        break;
 
-//             case MENU_QRDS:
-//                 //switch to QRD value view
-//                 state_ = S_QRDCHECK;
-//                 break;
+    case S_EXIT:
+        Serial.println("display - S_EXIT");
+        LCD.clear(); LCD.home();
+        LCD.setCursor(0, 0); LCD.print("<START> to GO!!!");
+        LCD.setCursor(0, 1); LCD.print("<STOP> to go back");
+        break;
 
-//             case MENU_IR:
-//                 //switch to QRD value view
-//                 state_ = S_IRCHECK;
-//                 break;
+    default: break;
+    }
+}
 
-//             case MENU_EXIT:
-//                 //switch to QRD value view
-//                 state_ = S_EXIT;
-//                 break;
+Menu::Menu(){
+    state_ = S_CYCLE;
+    menuOption_ = 0;
+    pdOption_ = 0;
+    counter = 0;
+    quitMenu = false;
 
-//             default: break;
-//             }
-//         }
-//         break;
+    LCD.clear(); LCD.home();
+    LCD.setCursor(0, 0); LCD.print("Starting up");
+    LCD.setCursor(0, 1); LCD.print("Press START");
+}
 
-//     case S_PDSET:
-//         //start to cycle through kp, kd, gain
-//         //stop to go back
-//         if (input == BTN_START) 
-//             (pdOption < PD_OPTIONS - 1) ? pdOption++ : pdOption = 0;
-//         else if (input == BTN_STOP) state_ = S_CYCLE;
-//         break;
+void Menu::handleInput(int input){
+    switch (state_){
+    case S_CYCLE:
+        if (input == BTN_START)
+            (menuOption_ < NUM_OPTIONS - 1) ? menuOption_++ : menuOption_ = 0;
+        else if (input == BTN_STOP){
+            switch (menuOption_){
+            case MENU_START: //if it is on the start menu, do nothing
+                break;
 
-//     case S_QRDCHECK:
-//         if (input == BTN_STOP) state_ = S_CYCLE;
-//         break;
+            case MENU_PDSET:
+                //switch to PD set menu
+                state_ = S_PDSET;
+                break;
 
-//     case S_IRCHECK:
-//         if (input == BTN_STOP) state_ = S_CYCLE;
-//         break;
+            case MENU_QDISP:
+                state_ = S_QRDDISP;
+                break;
 
-//     case S_EXIT:
-//         if (input == BTN_START) quitMenu = true;
-//         else if (input == BTN_STOP) state_ = S_CYCLE;
-//         break;
+            case MENU_QRDSET:
+                //switch to QRD value view
+                state_ = S_QRDSET;
+                break;
 
-//     default: break;
-//     }
+            case MENU_IR:
+                //switch to QRD value view
+                state_ = S_IRCHECK;
+                break;
 
-//     display();
-// }
+            case MENU_EXIT:
+                //switch to QRD value view
+                state_ = S_EXIT;
+                break;
+
+            default: break;
+            }
+        }
+        break;
+
+    case S_PDSET:
+        //start to cycle through kp, kd, gain
+        //stop to go back
+        if (input == BTN_START) 
+            (pdOption_ < PD_OPTIONS - 1) ? pdOption_++ : pdOption_ = 0;
+        else if (input == BTN_STOP) state_ = S_CYCLE;
+        break;
+
+    case S_QRDDISP:
+        if(input == BTN_START)
+            (qrdOption_ < NUM_QRDS - 1) ? qrdOption_ ++ : qrdOption_ = 0;
+        else if (input == BTN_STOP) state_ = S_CYCLE;
+        break;
+
+    case S_QRDSET:
+        //three sets of qrds
+        if (input == BTN_START)
+            (qrdPair_ < NUM_PAIRS - 1) ? qrdPair_ ++ : qrdPair_ = 0;
+        else if (input == BTN_STOP){
+            switch(qrdPair_){
+                case PAIR_OUT:
+                    //TODO: set qrd threshold
+                    break;
+
+                case PAIR_IN:
+                    //TODO: set
+                    break;
+                
+                case PAIR_EDGE:
+                    //TODO: set
+                    break;
+
+                case PAIR_EXIT:
+                    state_ = S_CYCLE;
+                    break;
+                
+                default: break;
+            }
+        }
+        break;
+
+    case S_IRCHECK:
+        if (input == BTN_STOP) state_ = S_CYCLE;
+        break;
+
+    case S_EXIT:
+        if (input == BTN_START) quitMenu = true;
+        else if (input == BTN_STOP) state_ = S_CYCLE;
+        break;
+
+    default: break;
+    }
+
+    display();
+}
--- robo_fett_V2/Main/Menu.h
+++ robo_fett_V2/Main/Menu.h
@@ -13,37 +13,49 @@ const int BTN_STOP = 0;
 
 const int KNOB = 0; //analog port 0
 
-//number of menu options -- SHOULD BE NUMBER OF NAMES
-const int NUM_OPTIONS = 5;
-const int PD_OPTIONS = 3; //kp, kd, gain
+//menuOption
+const int MENU_START  = 0;
+const int MENU_PDSET  = 1;
+const int MENU_QRDSET = 2;
+const int MENU_QDISP  = 3;
+const int MENU_IR     = 4;
+const int MENU_EXIT   = 5;
+const int NUM_OPTIONS = 6; //keep this last!! is num of options
+
+//pdOption
+const int PD_KP   = 0;
+const int PD_KD   = 1;
+const int PD_GAIN = 2;
+const int PD_OPTIONS = 3; //keep this last!! is num of options
+
+//qrdOption (from left to right)
+const int QRD_TAPE  = 0;
+const int QRD_EDGE  = 1;
+const int NUM_QRDS  = 2; //keep this last!! num of qrds
+
+//qrdPair (for qrdSet)
+const int PAIR_OUT  = 0;
+const int PAIR_IN   = 1;
+const int PAIR_EDGE = 2;
+const int PAIR_EXIT = 3;
+const int NUM_PAIRS = 4;
 
 class Menu{
 private:
     enum State{
         S_CYCLE,
         S_PDSET,
-        S_QRDCHECK,
+        S_QRDSET,
+        S_QRDDISP,
         S_IRCHECK,
         S_EXIT
     };
-
-    //names for menu items
-    enum menuOption{
-        MENU_START = 0,
-        MENU_PDSET = 1,
-        MENU_QRDS  = 2,
-        MENU_IR    = 3,
-        MENU_EXIT  = 4
-    };
-
-    //names for the PD set menu options
-    enum pdOption{
-        PD_KP = 0,
-        PD_KD = 1,
-        PD_GAIN = 2
-    };
-
+    
     State state_;
+    int menuOption_;
+    int pdOption_;
+    int qrdOption_;
+    int qrdPair_;
     int counter;
 
     void display();
